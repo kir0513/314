4b6ef6dfd22f2140cfa532eb2199b1cd92391691
kir0513 <kir0513@yandex.ru>
2024-04-14 17:17:50 +0300

    Изменить страницы вашего приложения в соответствии со скриншотами, используя Bootstrap.

---
+++ src/main/java/com/example/bootstrap_314/Bootstrap314Application.java
@@ -0,0 +1,13 @@
+package com.example.bootstrap_314;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootstrap314Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap314Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootstrap_314/Bootstrap314ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootstrap_314;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootstrap314ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
